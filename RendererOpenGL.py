7424eb79b449f9a93b5fcc61acff6794b9510316
Maria Vasquez <maria.ines.vf@gmail.com>
2020-11-23 19:51:39 -0600

    multiple objs done

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -8,11 +8,9 @@ Main
 
 import pygame
 from pygame.locals import *
-
-from gl import Renderer
+import glm
+from gl import Renderer, Model
 import shaders
-
-
 
 deltaTime = 0.0
 
@@ -24,24 +22,55 @@ screen = pygame.display.set_mode(screenSize, DOUBLEBUF | OPENGL)
 
 # Inicializacion de nuestro Renderer en OpenGL
 r = Renderer(screen)
+r.camPosition.z = 3
+r.pointLight.x = 5
+
 r.setShaders(shaders.vertex_shader, shaders.fragment_shader)
-r.createObjects()
 
-#constantes para mover el cubo en el espacio
-cubeX = 0
-cubeZ = 0
+fox= Model('fox.obj', 'fox.bmp')
+fox.position = glm.vec3(0,-30,-120)
+fox.rotation = glm.vec3(0, -90, 0)
 
-#ciclo de juego 
+nave = Model('space-shuttle-orbiter.obj', 'planespace.bmp')
+
+nave.position = glm.vec3(0,-30,-1000)
+nave.rotation = glm.vec3(0, -90, 0)
+
+tiger = Model('tiger.obj', 'tiger-atlas.bmp')
+tiger.position = glm.vec3(0,-30,-500)
+tiger.rotation = glm.vec3(0, -90, 0)
+
+mill = Model('ukulele.obj', 'uk.jpg')
+mill.position = glm.vec3(0,-30,-800)
+mill.rotation = glm.vec3(0, 0, 0)
+
+
+r.modelList.append(tiger)
+r.modelList.append(mill)
+r.modelList.append(Model('model.obj', 'model.bmp'))
+r.modelList.append(nave)
+#r.modelList.append(Model('marsRobot.obj', 'metal.bmp'))"""
+r.modelList.append(fox)
+
+
 isPlaying = True
 while isPlaying:
 
     # Para revisar si una tecla esta presionada
-    #funciona con las teclas direccionales <- y ->
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        cubeX -= 2 * deltaTime
-    if keys[pygame.K_RIGHT]:
-        cubeX += 2 * deltaTime
+
+    # Move cam
+    if keys[K_RIGHT]:
+        r.camPosition.x += 1 * deltaTime
+    if keys[K_LEFT]:
+        r.camPosition.x -= 1 * deltaTime
+    
+    
+    """if keys[K_w]:
+        r.camPosition.z -= 1 * deltaTime
+    if keys[K_s]:
+        r.camPosition.z += 1 * deltaTime"""
+
 
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
@@ -51,26 +80,24 @@ while isPlaying:
             if ev.key == pygame.K_1:
                 r.filledMode()
             elif ev.key == pygame.K_2:
-                r.wireframeMode
+                r.wireframeMode()
             elif ev.key == pygame.K_ESCAPE:
                 isPlaying = False
-        #para mover el cubo de atrás hacia adelante con la rueda del mouse
+            elif ev.key == pygame.K_SPACE:
+                r.activeModelIndex = (r.activeModelIndex+1)%len(r.modelList)
+        
         if ev.type == pygame.MOUSEBUTTONDOWN or ev.type == pygame.MOUSEBUTTONUP:
             if ev.button == 4:
-               cubeZ -= 2 * deltaTime
+               r.camPosition.z -= 1 * deltaTime
             if ev.button == 5:
-               cubeZ += 2 * deltaTime
-
-    #movimiento de cubo
-    r.translateCube(cubeX, 0, cubeZ)
+               r.camPosition.z += 1 * deltaTime
 
     # Main Renderer Loop
     r.render()
 
-    #para renderizar cada cierto tiempo
     pygame.display.flip()
     clock.tick(60)
     deltaTime = clock.get_time() / 1000
 
-#cuando renuncia del juego
+
 pygame.quit()
